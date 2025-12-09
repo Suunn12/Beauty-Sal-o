2953d8729f58cb676716aabf3758640d0519a791
Suunn12 <danielmirandagenovez@gmail.com>
2025-12-09 13:27:32 -0300

    Update Beauty_Conexao.js

--- js/Beauty_Conexao.js
+++ js/Beauty_Conexao.js
@@ -33,3 +33,117 @@ async function carregar_nomes(){
 }
 
 carregar_nomes();
+
+//Verifica os campos do formulário de serviços
+async function verificar_campos(){
+	let valido = true;
+	
+	//valida o nome - Caixa de texto
+	const nome_f = document.getElementById("nome");
+	const nome_selecionar = document.getElementById("lista_nomes");
+	
+	if(nome_f.value.trim() === ""){
+		//valida o nome - Caixa de lista
+		if(lista_nomes.value == "vazio"){
+			valido = false;
+			//console.log("o nome precisa estar preenchido!" + valido);
+		}else{
+			nome_f.value = nome_selecionar.value;
+			//console.log("nome selecionado!" + valido);
+		}
+	}else{
+		const {data, error} = await banco
+		.from("clientes")
+		.select("nome")
+		.eq("nome", nome_f.value);
+		
+		if(data.length > 0){
+			//console.log("Cliente já existe");
+			return true;
+		}
+		
+		if(error){
+			console.log("erro ao encontrar cliente");
+		}
+		
+		const {error: insertError} = await banco
+			.from("clientes")
+			.insert([{nome: nome_f.value}]);
+			console.log("Cliente adicionado");
+			
+		//console.log("nome preenchido!" + valido);
+	}
+	
+	
+	//valida o serviço
+	const servico_f = document.getElementById("lista_servicos");
+	
+	if(servico_f.value == "vazio"){
+		valido = false;
+		//console.log("serviço precisa ser selecionado!" + valido);
+	}else{
+		//console.log("serviço selecionado!" + valido);
+	}
+	
+	
+	//valida a data e hora
+	const data_f = document.getElementById("data");
+	const hora_f = document.getElementById("hora");
+	
+	if(data_f.value.trim() === ""){
+		valido = false;
+		//console.log("a data precisa ser preenchida!" + valido);
+	}else{
+		//console.log("data preenchida!" + valido);
+	}
+	
+	if(hora_f.value.trim() === ""){
+		valido = false;
+		//console.log("a hora precisa ser preenchida!" + valido);
+	}else{
+		//console.log("hora preenchida!" + valido);
+	}
+	
+	
+	//valida a forma de pagamento e parcelas
+	const pagamento_f = document.getElementById("lista_pagamento");
+	const parcelas_f = document.getElementById("lista_parcelas");
+	
+	if(pagamento_f.value == "vazio"){
+		valido = false;
+		//console.log("a forma de pagamento precisa ser preenchida!" + valido);
+		console.log("vazio");
+	}else if(pagamento_f.value == "credito"){
+		//console.log("forma de pagamento preenchida!" + valido);
+		//console.log(pagamento.value + " - " + parcelas.value);
+	}else{
+		parcelas_f.value = "1x";
+		//console.log("forma de pagamento preenchida!" + valido);
+		//console.log(pagamento.value + " - " + parcelas.value);
+	}
+	
+	
+	if(valido == true){
+		enviar_valores();
+		console.log("enviando para o banco de dados...");
+		
+		const {data, error} = await banco
+			.from("servicos")
+			.insert([{nome: nome_f.value, servico: servico_f.value, data: data_f.value, hora: hora_f.value, pagamento: pagamento_f.value, parcelas: parcelas_f.value}]);
+
+		//if(error) return log(error.message)
+		console.log(data);
+	
+	}
+	console.log(valido);
+}
+
+//envia para o banco de dados os valores preenchidos caso esteja tudo valido
+async function enviar_valores(){
+}
+
+//Botões
+document.getElementById("btn_confirmar").addEventListener("click", function(e){
+	e.preventDefault();
+	verificar_campos();
+});
